c9d7ee782f43f622ae21d984e76daa86aedf9f1b
David Lehman <dlehman@redhat.com>
2018-06-15 12:20:25 -0400

    Refactor resolve_blockdev to enable unit testing.
    Also add support for resolving by basenames for device-mapper, md,
    and /dev/disk/ symlink.

--- library/resolve_blockdev.py
+++ library/resolve_blockdev.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python
 
+import glob
+import os
+
 from ansible.module_utils.basic import AnsibleModule
 
-import os
+
+SEARCH_DIRS = ['/dev', '/dev/mapper', '/dev/md'] + glob("/dev/disk/by-*")
+
+
+def resolve_device(spec, run_cmd):
+    if "=" in spec:
+        device = run_cmd("blkid -t %s -o device" % spec)[1].strip()
+    elif not spec.startswith('/'):
+        for devdir in SEARCH_DIRS:
+            device = "%s/%s" % (devdir, spec)
+            if os.path.exists(device):
+                break
+            else:
+                device = ''
+    else:
+        device = spec
+
+    return device
+
 
 def run_module():
     module_args = dict(
@@ -18,15 +39,7 @@ def run_module():
         supports_check_mode=True
     )
 
-    if "=" in module.params['spec']:
-        result['device'] = os.popen("blkid -t %s -o device" % module.params['spec']).read().strip()
-    elif not module.params['spec'].startswith('/'):
-        result['device'] = "/dev/%s" % module.params['spec']
-        if not os.path.exists(result['device']):
-            result['device'] = None
-    else:
-        result['device'] = module.params['spec']
-
+    result['device'] = resolve_device(module.params['spec'], run_cmd=module.run_command)
     if not os.path.exists(result['device']):
         module.fail_json(msg="The {} device spec could not be resolved".format(module.params['spec']))
 
